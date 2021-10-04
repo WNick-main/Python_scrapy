7163f02d8b7483c728b990c20074334a8e2c90f9
wnick <wnick@bk.ru>
2021-10-04 19:23:59 +0300

    -

--- Lesson 1/HW.py
+++ Lesson 1/HW.py
@@ -3,31 +3,10 @@ import json
 from pprint import pprint
 
 
-def ex1():
+def ex():
     """
     1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
     сохранить JSON-вывод в файле *.json.
-    """
-    user = 'wnick-main'
-    token = ''
-    url = 'https://api.github.com/user/repos'
-
-    responce = requests.get(url, auth=(user, token))
-
-    if responce.ok:
-        d_resp = responce.json()
-        for item in d_resp:
-            pprint(item.get('name'))
-
-        with open("repos.json", "w") as repos:
-            json.dump(d_resp, repos)
-
-
-ex1()
-
-
-def ex2():
-    """
     2. Изучить список открытых API. Найти среди них любое, требующее авторизацию (любого типа).
     Выполнить запросы к нему, пройдя авторизацию.
     Ответ сервера записать в файл.
@@ -36,15 +15,16 @@ def ex2():
     token = ''
     url = 'https://api.github.com/user/repos'
 
-    responce = requests.get(url, auth=(user, token))
+    response = requests.get(url, auth=(user, token))
 
-    if responce.ok:
-        d_resp = responce.json()
+    if response.ok:
+        d_resp = response.json()
         for item in d_resp:
             pprint(item.get('name'))
 
         with open("repos.json", "w") as repos:
             json.dump(d_resp, repos)
-            
 
-ex2()
+
+ex()
+
